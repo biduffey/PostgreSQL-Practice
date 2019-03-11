547fd802920fbe602891d6452bfb41f6deb1237d
bduffey <bduffey@tree.com>
2019-03-10 19:06:37 -0700

    adding in Common Table Expressions (CTE's)`

--- Aggregations.sql
+++ Aggregations.sql
@@ -32,3 +32,28 @@ order by  1 ,extract('month' from starttime )
 ----------------------------------
 -- Find the total number of members who have made at least one booking.
 select count(distinct memid) from cd.bookings
+
+
+----------------------------------
+-- Produce a list of facilities with more than 1000 slots booked. Produce an output table consisting of facility id and hours, sorted by facility id.
+select facid
+		, sum(slots)
+from cd.bookings
+group by facid
+having sum(slots) > 1000
+order by facid
+
+
+----------------------------------
+-- Output the facility id that has the highest number of slots booked without using a LIMIT clause. This version will probably look messy!
+
+with total_slots as (
+select f.facid
+    , sum(slots) as slots
+from cd.bookings b
+  join cd.facilities f
+  on b.facid=f.facid
+group by f.facid )
+
+select facid,  slots from total_slots where slots = (select max(slots) from total_slots)
+---------------------------
